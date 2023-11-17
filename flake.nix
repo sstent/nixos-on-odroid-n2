9f35e480ab5902c990cb540ae53370198743b6b4
sstent <stuart.stent@gmail.com>
2023-11-17 16:12:17 +0000

    fixing flake - adding stuff back

--- flake.nix
+++ flake.nix
@@ -55,8 +55,8 @@
 
           #boot.loader.grub.enable = false;
           #boot.loader.kboot-conf.enable = true;
-          boot.kernelParams = [ "console=ttyAML0,115200n8" ];
-          boot.consoleLogLevel = 7;
+          #boot.kernelParams = [ "console=ttyAML0,115200n8" ];
+          #boot.consoleLogLevel = 7;
           # need latest kernel for the n2+ device tree blob
           boot.kernelPackages = pkgs.linuxPackages_latest;
           #boot.kernelPackages = lib.mkForce config.boot.zfs.package.latestCompatibleLinuxPackages;
