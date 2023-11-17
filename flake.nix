a2dabd5d8552b329c030cb15fe6bf255825c0f3f
sstent <stuart.stent@gmail.com>
2023-11-17 15:43:46 +0000

    fixing flake

--- flake.nix
+++ flake.nix
@@ -31,7 +31,8 @@
         {
           imports = [
             #./sd-image.nix
-            ./default.nix
+            # ./default.nix
+            ./kboot-conf
             "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
 
           ];
@@ -49,6 +50,15 @@
               ${config.boot.loader.kboot-conf.populateCmd} -c ${config.system.build.toplevel} -d ./files/kboot.conf
             '';
             };
+
+          boot.loader.grub.enable = false;
+          boot.loader.kboot-conf.enable = true;
+          boot.kernelParams = [ "console=ttyAML0,115200n8" ];
+          boot.consoleLogLevel = 7;
+          # need latest kernel for the n2+ device tree blob
+          boot.kernelPackages = pkgs.linuxPackages_latest;
+
+
           services.openssh = {
             enable = true;
             settings.PermitRootLogin = "yes";
